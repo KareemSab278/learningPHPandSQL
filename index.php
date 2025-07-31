79ddfa8f5358cad866de580a954d0cdfd7b16dee
KareemSab278 <10288694@student.walsallcollege.ac.uk>
2025-07-31 19:08:12 +0100

    learning some basics still

--- index.php
+++ index.php
@@ -1,2 +1,36 @@
-<!-- following tutorial from bro code: https://www.youtube.com/watch?v=zZ6vybT1HQs&t -->
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="get"> <!-- action should be the file name and the method should be declared -->
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" name="exampleInputEmail1" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" name="exampleInputPassword1" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+
+</html>
+
+<?php
+echo $_GET["exampleInputEmail1"]; // this is technically an array and we need to get the username
+echo "<br>";
+echo $_GET["exampleInputPassword1"];
+// this data unfortunately is appended to the url tho so it isnt safe.
+?>
---
+++ introduction.php
@@ -0,0 +1,49 @@
+<!-- following tutorial from bro code: https://www.youtube.com/watch?v=zZ6vybT1HQs&t -->
+<!-- run on: https://localhost/phpsql/index.php -->
+<!-- i hate xampp like bruh is it xaamp xammp or xampp??? -->
+<!-- alt shift f to format dont forget lol -->
+<!-- you cant miss semicolons!; -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>i am some text</p>
+</body>
+
+</html>
+
+<?php
+
+echo "hello world <br>";
+echo "it is really good <br>";
+//  this is a comment
+// imma declare a var rn
+
+
+$nullVal = null; // you can create a var and add null to it to reassign ltr
+$nullVal = 1 + 2;
+echo "null value is: \${$nullVal}"; // use an exc sequence \
+
+$firstVar = "i am the first var";
+echo "<br> hello, {$firstVar}"; // you can concat literals and vars
+
+
+
+//lets try a fizz buzz ltr lol ( divisible by 3 is fizz divisible by 5 is buzz divisible by 3 and 5 is fizzbuzz)
+
+// need to write a function
+
+// write a loop
+
+// write conditions and outputs
+
+
+?>
